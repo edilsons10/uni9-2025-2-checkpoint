a076a70ce4491e90712ec7e5fb7ed5d0eadae281
edilsons10 <edilsonmanoel952@gmail.com>
2025-11-05 22:41:42 +0000

    feat: app

--- app/WhatsApp.cs
+++ app/WhatsApp.cs
@@ -1,27 +1,30 @@
 public class WhatsApp
 {
-    public void EnviarMensagem(string numero,TextMessage message)
+    public void EnviarMensagem(string numero, TextMessage message)
     {
-        Console.WritiLine($"Enviando mensagem de texto para o numero{numero}");
-    }
-
-    
- public void EnviarMensagem(string numero,VideoMessage message)
-{
-    Console.WritiLine($"Enviando mensagem de video para o numero{numero}");
-}
-
-    
-     public void EnviarMensagem(string numero,PhotoMessage message)
-{
-    Console.WritiLine($"Enviando mensagem de foto para o numero{numero}");
-}
-
-    
-     public void EnviarMensagem(string numero,FileMessage message)
-{
-    Console.WritiLine($"Enviando mensagem de arquivo para o numero{numero}");
-}
-
+        Console.WriteLine($"Enviando mensagem de texto para o número {numero}");
+        Console.WriteLine($"Mensagem: {message.Message}");
 
     }
+
+    public void EnviarMensagem(string numero, VideoMessage message)
+    {
+        Console.WriteLine($"Enviando mensagem de video para o número {numero}");
+                Console.WriteLine($"Mensagem: {message.Message}");
+
+    }
+
+    public void EnviarMensagem(string numero, PhotoMessage message)
+    {
+        Console.WriteLine($"Enviando mensagem de foto para o número {numero}");
+                Console.WriteLine($"Mensagem: {message.Message}");
+
+    }
+
+     public void EnviarMensagem(string numero, FileMessage message)
+    {
+        Console.WriteLine($"Enviando mensagem de arquivo para o número {numero}");
+                Console.WriteLine($"Mensagem: {message.Message}");
+
+    }
+}
